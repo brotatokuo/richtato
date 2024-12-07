7de152bc1e247057f9f6ce2d4b51e4a50251c164
brotatokuo <kuoalan9@gmail.com>
2024-12-07 13:55:28 -0800

    updated update.bat

--- update.bat
+++ update.bat
@@ -1,10 +1,12 @@
 @echo off
-cd /d ""  REM Change this to your repository path
+echo Current directory: %~dp0
+cd /d "%~dp0"  || (echo Failed to change directory! & pause & exit /b)
 
 echo Fetching latest changes from the remote repository...
-git fetch
+git fetch || (echo Failed to fetch from Git! & pause & exit /b)
 
 echo Pulling the latest changes from the remote repository...
-git pull
+git pull || (echo Failed to pull from Git! & pause & exit /b)
 
 echo Done!
+pause
