69b0f1acd818ad32ede93540cfc5590e851b918d
Alan Kuo <161139744+brotatokuo@users.noreply.github.com>
2025-02-28 21:44:06 -0800

    update run

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo "Starting Django Server"
 python richtato/manage.py makemigrations
+echo "Migrations made"
 python richtato/manage.py migrate
+echo "Migrations applied"
+echo "starting server"
+ls
 python richtato/manage.py runserver 0.0.0.0:10000
