107ca33a875c24f32a2e0956246276c713ffe52a
brotatokuo <kuoalan9@gmail.com>
2025-03-18 07:59:35 -0700

    reorg js

--- richtato/static/js/input.js
+++ richtato/static/js/input.js
@@ -1,10 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Add event listener to description
-    const descriptionInput = document.getElementById('expense-description');
-    descriptionInput.addEventListener('blur', () => {
-        const description = descriptionInput.value;
-        guessCategoryFromDescription(description);
+const currencyInputs = document.getElementsByClassName('currency-input');
+console.log('Currency inputs:', currencyInputs);
+Array.from(currencyInputs).forEach(currencyInput => {
+
+    currencyInput.addEventListener('input', () => {
+        const value = currencyInput.value;
+        const validCharacters = /^[0-9+\-*/().=]*$/;
+        if (!validCharacters.test(value)) {
+            currencyInput.value = value.replace(/[^0-9+\-*/().=]/g, '');
+        }
     });
+
+    currencyInput.addEventListener('blur', () => {
+        let balance = currencyInput.value;
+
+        if (balance) {
+            let newBalance = computeBalance(balance);
+
+            if (newBalance) {
+                currencyInput.value = newBalance;
+                console.log('New balance:', newBalance);
+            }
+        }
+    });
+});
+
+const descriptionInput = document.getElementById('expense-description');
+descriptionInput.addEventListener('blur', () => {
+    const description = descriptionInput.value;
+    guessCategoryFromDescription(description);
 });
 
 function guessCategoryFromDescription(description) {
@@ -28,4 +51,21 @@ function guessCategoryFromDescription(description) {
         });
 
 
+}
+
+function computeBalance(balance) {
+    if (balance.startsWith('=')) {
+        try {
+            balance = eval(balance.slice(1));
+            console.log('Evaluated formula:', balance);
+        } catch (error) {
+            console.error('Invalid formula:', error);
+            return;
+        }
+    }
+    else {
+        balance = eval(balance);;
+    }
+    balance = parseFloat(balance).toFixed(2);
+    return balance;
 }
