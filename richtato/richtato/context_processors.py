3f948b581a6e8c23eccc47828a5035267293934b
brotatokuo <kuoalan9@gmail.com>
2025-03-29 10:29:31 -0700

    fixed budget title

--- richtato/richtato/context_processors.py
+++ richtato/richtato/context_processors.py
@@ -10,6 +10,6 @@ def date(request: HttpRequest) -> dict:
     current_date = datetime.now()
     return {
         "current_year": str(current_date.year),
-        "current_month": str(current_date.strftime("%b")),
+        "current_month": str(current_date.strftime("%B")),
         "current_day": str(current_date.day),
     }
