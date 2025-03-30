550e0b447c83b1915223baf17090306f8cef6655
brotatokuo <kuoalan9@gmail.com>
2025-03-30 14:01:25 -0700

    new login

--- richtato/static/js/welcome.js
+++ richtato/static/js/welcome.js
@@ -6,4 +6,15 @@ document.addEventListener("DOMContentLoaded", (event) => {
       gif.src = staticImagePath;
     }, 2000);
   }
+
+  const pageContent = document.querySelector(".page-content");
+  const sidebar = document.querySelector(".sidebar");
+
+  // Hide the sidebar by adding the "sidebar-hidden" class
+  function hideSidebar() {
+    sidebar.style.display = "none";
+    pageContent.classList.add("sidebar-hidden");
+  }
+  hideSidebar();
+
 });
