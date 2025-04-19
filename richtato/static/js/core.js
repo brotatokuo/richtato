7838aa9750eb15a9f6c436024f67a05319e3d7dc
Alan Kuo <161139744+brotatokuo@users.noreply.github.com>
2025-04-20 06:38:37 +0800

    remove welcome.js

--- richtato/static/js/core.js
+++ richtato/static/js/core.js
@@ -1,3 +1,47 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const userAuthenticated = "{{ user.is_authenticated|yesno:'true,false' }}";
+  console.log("user auth: ", userAuthenticated);
+
+  var gif = document.getElementById("growth-gif");
+  if (gif) {
+    setTimeout(function () {
+      document.querySelector(".typewriter-text").style.borderRight = "none";
+      gif.src = staticImagePath;
+    }, 2000);
+  }
+
+  const pageContent = document.querySelector(".page-content");
+  const sidebar = document.querySelector(".sidebar");
+
+  // Hide the sidebar by adding the "sidebar-hidden" class
+  function hideSidebar() {
+    sidebar.style.display = "none";
+    pageContent.classList.add("sidebar-hidden");
+  }
+
+  hideSidebar();
+  const hamburger = document.querySelector("#hamburger");
+  hamburger.style.display = "none";
+
+  const loginButton = document.querySelector("#login-button");
+  if (loginButton) {
+    loginButton.addEventListener("keypress", (event) => {
+      if (event.key === "Enter") {
+        loginButton.click();
+      }
+    });
+  }
+
+  const registerButton = document.querySelector("#register-button");
+  if (registerButton) {
+    registerButton.addEventListener("keypress", (event) => {
+      if (event.key === "Enter") {
+        registerButton.click();
+      }
+    });
+  }
+});
+
 function togglePasswordVisibility(id, button) {
   console.log("togglePasswordVisibility called with id:", id);
   var passwordInput = document.getElementById(id);
@@ -11,23 +55,25 @@ function togglePasswordVisibility(id, button) {
 }
 
 function getCSRFToken() {
-  const cookieValue = document.cookie.match('(^|;)\\s*csrftoken\\s*=\\s*([^;]+)')?.pop();
-  return cookieValue || '';
+  const cookieValue = document.cookie
+    .match("(^|;)\\s*csrftoken\\s*=\\s*([^;]+)")
+    ?.pop();
+  return cookieValue || "";
 }
 
 function getUserID() {
-  return fetch('/get-user-id')
-    .then(response => response.json())
-    .then(data => data.userID);
+  return fetch("/get-user-id")
+    .then((response) => response.json())
+    .then((data) => data.userID);
 }
 
 function computeBalance(balance) {
-  if (balance.startsWith('=')) {
+  if (balance.startsWith("=")) {
     try {
       balance = eval(balance.slice(1));
-      console.log('Evaluated formula:', balance);
+      console.log("Evaluated formula:", balance);
     } catch (error) {
-      console.error('Invalid formula:', error);
+      console.error("Invalid formula:", error);
       return;
     }
   }
