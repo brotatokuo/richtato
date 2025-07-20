274a3a4b234f9427cacec1e2ca28f5b65164d59d
brotatokuo <kuoalan9@gmail.com>
2025-07-19 22:07:44 -0700

    remove extra js

--- richtato/static/js/pages/graph.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", async () => {
-    const timeseriesGraph = new TimeseriesGraph('cashFlowLineChart', '/api/timeseries-data/');
-    timeseriesGraph.init();
-});
