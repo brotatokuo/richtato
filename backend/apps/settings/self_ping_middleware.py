f5eb18363af33db112354f1073e970e94cd072c9
Alan Kuo <161139744+brotatokuo@users.noreply.github.com>
2025-11-24 09:26:19 -0800

    new endpoint

--- backend/apps/settings/self_ping_middleware.py
+++ backend/apps/settings/self_ping_middleware.py
@@ -15,7 +15,7 @@ class SelfPingMiddleware:
     def ping_self_periodically(self):
         while self.keep_running:
             try:
-                new_prod = "https://richtato-latest.onrender.com"
+                new_prod = "https://richtato.com"
                 response = requests.get(new_prod)
                 print(f"Pinged {new_prod} Status code: {response.status_code}")
 
